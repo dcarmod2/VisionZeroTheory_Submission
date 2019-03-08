8b6a6566d2598938dddddacfe70583e7a29abece
Daniel Carmody <danielcarmody@wirelessprv-10-195-130-185.near.illinois.edu>
2019-03-08 15:36:16 -0600

    removing manhattan folders

--- FINAL_Manhattan_Weekday/windower.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-name="Manhattan weekdays"
-print(name)
-
-def timewindower(dt):
-    if (dt.isoweekday()>5):
-    	return False #immediate return false if weekend
-    return True
--- FINAL_Manhattan_Weekend/windower.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-name="Manhattan weekend"
-print(name)
-
-def timewindower(dt):
-    if (dt.isoweekday()>5):
-    	return True #immediate return true if weekend
-    return False
